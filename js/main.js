73d8b8ea0b0c97de8a970ac09a44a6bd23de17eb
CASAMSON <sogbonna2@gmail.com>
2018-02-27 19:46:55 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var todo = new Vue({
+	el:'#todo',
+	data: {
+		newTask:'',
+		todoList: []
+	},
+	methods:{
+		addTask: function(){
+			var task = this.newTask;
+			this.todoList.push(task);
+			this.newTask='';
+		},
+		removeTask: function(task) {
+			var index = this.todoList.indexOf(task);
+			this.todoList.splice(index, 1);
+		}
+	}
+});
